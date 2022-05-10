1fea9a930cce515c889063523c122eb471476aa7
Thistleman <codes5096@gmail.com>
2022-05-10 14:31:21 -0700

    Added required line to use script in bash

--- openfidodl.sh
+++ openfidodl.sh
@@ -1,4 +1,8 @@
-
+#!/bin/bash
+#
+# Standard Script call to install and setup node
+# and to download all files in provided urls
+#
 
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 apt-get -q -y install nodejs > /dev/null
