71f8e43568c9460f8474ee3bef46ae02c892e5dd
Thistleman <codes5096@gmail.com>
2022-05-10 10:33:06 -0700

    Updated script

--- hello.js
+++ hello.js
@@ -1,5 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+
 process.argv.forEach(function (val, index, array) {
     console.log(index + ': ' + val);
+    fs.readdir(val, function (err, files) {
+        //handling error
+        if (err) {
+            return console.log('Unable to scan directory: ' + err);
+        } 
+        //listing all files using forEach
+        files.forEach(function (file) {
+            // Do whatever you want to do with the file
+            console.log(file); 
+        });
+    });
   });
 
 ((inputs,outputs,options) => {
@@ -7,4 +21,3 @@ process.argv.forEach(function (val, index, array) {
     console.log(inputs, outputs, options);
     return "Hello, console";
 })();
-
